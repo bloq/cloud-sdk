39b539ad98510fd4ed5926167bdbba9129b0ae49
ndelvalle <nicolas.delvalle@bloq.com>
2019-07-07 19:54:24 -0300

    Implement socket.io wrapper

--- src/insight/socket.js
+++ src/insight/socket.js
@@ -1,10 +1,9 @@
 'use strict'
 
 const io = require('socket.io-client')
+const pDefer = require('p-defer')
 const config = require('../../config')
 const { auth: createAuthClient } = require('../../')
-
-const JWT_EXPIRED_ERROR = 'jwt expired'
 
 function createClient ({
   coin = 'btc',
@@ -19,30 +18,90 @@ function createClient ({
     )
   }
 
+  const client = {}
+  const subscriptions = []
   const authClient = createAuthClient(auth)
-  const socket = io(url, {
-    query: {
-      token: auth.accessToken
+
+  client.connect = function () {
+    const deferred = pDefer()
+
+    authClient.accessToken()
+      .then(function (token) {
+        client.socket = io(url, { query: { token } })
+
+        client.socket.on('connect', deferred.resolve)
+        client.socket.on('error', deferred.reject)
+        client.socket.on('disconnect', function (reason) {
+          if (reason !== 'io server disconnect') {
+            // Note: The socket will automatically try to reconnect
+            return
+          }
+
+          // Note: the disconnection was initiated by the server, we need to
+          // reconnect manually
+          authClient.accessToken()
+            .then(function (token) {
+              client.socket.io.opts.query = { token }
+              client.socket.connect()
+            })
+            .catch(function (err) {
+              throw err
+            })
+        })
+      })
+      .catch(deferred.reject)
+
+    return deferred.promise
+  }
+
+  client.isConnected = function () {
+    return client.socket && client.socket.connected
+  }
+
+  client.onTransaction = function (fn) {
+    if (!client.isConnected()) {
+      throw new Error(
+        'Failed subscribing to transactions, socket is disconnected'
+      )
     }
-  })
 
-  socket.on('reconnect_attempt', function () {
-    authClient.accessToken(function (token) {
-      socket.io.opts.query = { token }
-    })
-  })
-
-  socket.on('disconnect', function (reason) {
-    if (reason !== JWT_EXPIRED_ERROR) {
-      return
+    if (!subscriptions.includes('inv')) {
+      client.socket.emit('subscribe', 'inv')
+      subscriptions.push('inv')
     }
-    authClient.accessToken(function (token) {
-      socket.io.opts.query = { token }
-      socket.connect()
-    })
-  })
+    client.socket.on('tx', fn)
+  }
 
-  return socket
+  client.onBlock = function (fn) {
+    if (!client.isConnected()) {
+      throw new Error(
+        'Failed subscribing to blocks, socket is disconnected'
+      )
+    }
+
+    if (!subscriptions.includes('inv')) {
+      client.socket.emit('subscribe', 'inv')
+      subscriptions.push('inv')
+    }
+    client.socket.on('block', fn)
+  }
+
+  client.onAddressTransaction = function (address, fn) {
+    if (!client.isConnected()) {
+      throw new Error(
+        'Failed subscribing to address transactions, socket is disconnected'
+      )
+    }
+
+    if (!subscriptions.includes(address)) {
+      client.socket.emit('subscribe', 'bitcoind/addresstxid', address)
+      subscriptions.push(address)
+    }
+
+    client.socket.on(address, fn)
+  }
+
+  return client
 }
 
 module.exports = createClient
